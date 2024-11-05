494c5681ab39f3cf04ce043e2fe04b9ff5465e8a
Ritik Gupta <142598621+ritikgupta06@users.noreply.github.com>
2024-11-05 20:10:50 +0530

    images added

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,10 @@
-import Head from "next/head";
-import Image from "next/image";
-import localFont from "next/font/local";
-import styles from "@/styles/Home.module.css";
-import Navbar from "@/components/navbar";
+
+import HomePage from "./home";
 
 export default function Home() {
   return (
     <>
-     <Navbar/>
+    <HomePage/>
     </>
   );
 }
